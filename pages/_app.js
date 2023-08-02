d590b58faff3707a1852d019604d96c4e271aea9
Kilian Baur <baurkilian@yahoo.de>
2023-08-02 16:47:35 +0200

    add favorite-button

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,31 @@
 import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
 import useSWR from "swr";
+import useLocalStorageState from "use-local-storage-state";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 const URL = "https://example-apis.vercel.app/api/art";
 
 export default function App({ Component, pageProps }) {
   const { data, isLoading, error } = useSWR(URL, fetcher);
+
+  const [artPiecesInfo, setArtPiecesInfo] = useLocalStorageState(
+    "artPiecesInfo",
+    { defaultValue: [] }
+  );
+
+  function toggleFavorite(slug) {
+    const artPieceInfo = artPiecesInfo.find((piece) => piece.slug === slug);
+    if (artPieceInfo) {
+      setArtPiecesInfo(
+        artPiecesInfo.map((piece) =>
+          piece.slug === slug ? { slug, isFavorite: !piece.isFavorite } : piece
+        )
+      );
+    } else {
+      setArtPiecesInfo([...artPiecesInfo, { slug, isFavorite: true }]);
+    }
+  }
 
   if (isLoading) {
     return <h1>... is loading</h1>;
@@ -20,7 +39,12 @@ export default function App({ Component, pageProps }) {
     <>
       <GlobalStyle />
       <SWRConfig value={{ fetcher }}>
-        <Component data={data} {...pageProps} />
+        <Component
+          data={data}
+          {...pageProps}
+          artPiecesInfo={artPiecesInfo}
+          toggleFavorite={toggleFavorite}
+        />
       </SWRConfig>
     </>
   );
